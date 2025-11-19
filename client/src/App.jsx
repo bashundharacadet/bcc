66414af2f39b150ad75fe24f35ad2872203234ad
Md. Asad Chowdhary <asad@suyena.com>
2025-11-19 15:58:55 +0600

    Implement backend data fetching and CORS support; update environment configurations

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  // Fetch data from backend and display it here
+
+  const [message, setMessage] = useState('')
+
+  useEffect(()=>{
+    fetch('http://localhost:7445/hi')
+    .then(res => res.text())
+    .then(data => setMessage(data))
+    .catch(err => console.log(err))
+  }, [])
 
   return (
     <><h1>School App</h1>
+    <p>Data from backend: {message}</p>
+
     </>
   )
 }
--- server/index.js
+++ server/index.js
@@ -1,15 +1,20 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 dotenv.config()
 const PORT = process.env.PORT
 
 const app = express()
 app.use(express.json())
-
+app.use(cors())
 
 
 app.get('/', (req, res)=>{
     res.send('Server is running...')
+})
+
+app.get('/hi', (req, res)=>{
+    res.send('Hi from the backend!')
 })
 
 
